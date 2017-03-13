6b370211b4fe891392adddc5d69145d145a17e53
ITE99050375 <gas852164@gmail.com>
2017-03-13 19:26:02 +0800

    Add files via upload

---
+++ _01.ino
@@ -0,0 +1,22 @@
+const byte LED=13;
+char val;
+
+void setup()
+{
+Serial.begin(9600);
+Serial.println("Hello,");
+}
+
+void loop(){
+  if(Serial.available()){
+    val = Serial.read();
+    if(val=='1'){
+      digitalWrite(LED, HIGH);
+      Serial.print("LED ON\n");
+    } else if (val=='0'){
+     digitalWrite(LED, LOW);
+     Serial.print("LED OFF\n");
+  }
+}
+}
+
---
+++ hw-02.ino
@@ -0,0 +1,36 @@
+ 
+
+
+void setup() { 
+Serial.begin(9600); 
+
+}
+
+void loop() 
+{ 
+float sensorValue = 0; 
+float sensorVoltage = 0; 
+
+for(int i=1 ; i<6;i++ ) 
+{ 
+sensorValue = analogRead(A1); 
+sensorValue = 0.7* sensorValue + 0.3* analogRead(A0); 
+sensorVoltage = (sensorValue * 5) /1024; 
+//Databuffer[i] = sensorVoltage ; 
+}
+//Serial.print("sensorVoltage = ");
+//Serial.println(sensorVoltage); 
+//Serial.print("sensorValue = "); 
+//Serial.println(sensorValue);
+//delay(3000); 
+
+for(int i=0 ; i<5;i++ )
+{
+//Databuffer[i] = Databuffer[i+1];
+//a+=Databuffer[i];
+Serial.println(sensorVoltage); 
+} 
+}
+
+
+
